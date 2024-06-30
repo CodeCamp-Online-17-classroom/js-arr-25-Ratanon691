7c5b5b423f08fe6a6c6ce2bc42ce182b085820d6
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 18:38:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+const inventory = [
+  { name: 'apples', quantity: 2 },
+  { name: 'bananas', quantity: 0 },
+  { name: 'cherries', quantity: 5 }
+];
+function searchForCherries(arr, searchName) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name === searchName) {
+      console.log(arr[i])
+      return
+    }
+  }
+  console.log(`There is no ${searchName} in the inventory`)
+}
+
+
